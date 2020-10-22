8769e6ea7b2716d47d51be583edec90b0cbf6c1a
Jessy SOBREIRO <jessy.sobreiro@gmail.com>
2020-10-22 21:49:36 +0200

    Further cleaner logging

--- src/apcHandler.js
+++ src/apcHandler.js
@@ -22,8 +22,8 @@ class ApcHandler {
     }
     setSensitivityHigh() {
         exec("echo '5\n4\nH\nq\nq\n' | /sbin/apctest", (error, stdout, stderr) => {
-            if (error || stderr) {
-                this.error(`stderr: ${error || stderr}`);
+            if (error) {
+                this.error(`stderr: ${stderr}`);
                 return;
             }
             this.log("info: Set sensitivity to HIGH")
